62b218fa8054912ac9151f9aae3c6ffd50700b73
Ngoc Vu <SUN-ASTERISK\vu.thi.ngoc@b120484-lt.sun-asterisk.com>
2022-11-08 11:01:15 +0700

    init components

---
+++ src/pages/User/Home.jsx
@@ -0,0 +1,35 @@
+import BannerImage1 from '../../img/banner/banner-1.jpg';
+import BannerImage2 from '../../img/banner/banner-2.jpg';
+
+import Category from '../../components/Home/Category';
+import FeatureProduct from '../../components/Home/FeatureProduct';
+import LatestProduct from '../../components/Home/LatestProduct';
+import Blog from '../../components/Home/Blog';
+
+function Home(){
+  return(
+    <>
+      <Category />
+      <FeatureProduct />
+      <div className="banner">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6 col-md-6 col-sm-6">
+              <div className="banner__pic">
+                <img src={BannerImage1} alt="" />
+              </div>
+            </div>
+            <div className="col-lg-6 col-md-6 col-sm-6">
+              <div className="banner__pic">
+                <img src={BannerImage2} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <LatestProduct />
+      <Blog />
+    </>
+  );
+}
+export default Home;
